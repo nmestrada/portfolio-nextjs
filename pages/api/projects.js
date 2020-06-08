6eefb373aa1970f1e0f9e3de1067d6cf9834a468
Natalie Estrada <nmestrad@gmail.com>
2020-06-08 14:14:04 -0500

    added singleProject and image components

--- pages/api/projects.js
+++ pages/api/projects.js
@@ -5,19 +5,19 @@ export default (req, res) => {
             title: "WeJay",
             description: 'A mobile application',
             techStack: ['React Native','Redux','Expo SDK', 'Google Firebase', 'Spotify API', 'Node.js'],
-            images:[],
+            images:['/images/weJay1.png','/images/wejay2.png'],
         },
         {
             title: 'BeerHopper',
             description: 'An e-commerce site',
             techStack:['React', 'Redux', 'Express', 'Postgres', 'Sequelize ORM', 'Node.js', 'Heroku CLI', 'OAuth', 'Passport.js'],
-            images: []
+            images: ['/images/beerHopper1.png']
         },
         {
             title: 'Plantify',
             description: 'A mobile application to identify plants',
             techStack: ['React Native', 'Node.js','Expo SDK', 'Google Firebase', 'Clarifai API'],
-            images: []
+            images: ['/images/plantify1.jpg','/images/plantify2.jpg']
         }
     ])
   }
