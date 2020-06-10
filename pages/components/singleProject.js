d6ebe1411ffa96ef984122731e2af2484668e9e5
Natalie Estrada <nmestrad@gmail.com>
2020-06-10 18:16:14 -0500

    project cards mostly styled

--- pages/components/singleProject.js
+++ pages/components/singleProject.js
@@ -5,27 +5,26 @@ export default function SingleProject({project}) {
     return (
         <div 
             className="project card" 
-            style={{
-                backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${images[0]})`,
-                backgroundPosition: 'center',
-                backgroundSize: 'cover',
-                backgroundRepeat: 'no-repeat'
-            }}
             key={project.title}
         >
-            <div 
-                className="project-content"
-            >
+            <div className="project-content">
                 <h2>{project.title}</h2>
-                <h4>{project.description}</h4>
-                {/* <ul className="techStack">
-                    {techStack.map(item => <li key={item}>{item}</li>)}
-                </ul> */}
+                <p>{project.description}</p>
+                <p>Built With:
+                    <ul className="techStack">
+                        {techStack.map(item => <li key={item}>{item}</li>)}
+                    </ul>
+                </p>
+                
+            
+                <div className="project-links">
+                    <span>Github</span>
+                </div>
             </div>
-            {/* <div className='images-container'>
+            <div className='images-container'>
                 <img src={images[0]} alt='project-image' className='image'/>
-                {/* {images.map(image => <img key={image} src={image} alt='project-image' className='image'/>)} 
-            </div> */}
+                {/* {images.map(image => <img key={image} src={image} alt='project-image' className='image'/>)}  */}
+            </div>
         </div>
     )
 }
