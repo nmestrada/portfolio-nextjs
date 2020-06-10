31411f33a160ade858bc1c6e5d2d26d10b6ad21d
Natalie Estrada <nmestrad@gmail.com>
2020-06-10 12:37:52 -0500

    in the of styling project card views

--- pages/components/singleProject.js
+++ pages/components/singleProject.js
@@ -3,17 +3,29 @@ export default function SingleProject({project}) {
     const images = project.images;
     const techStack = project.techStack;
     return (
-        <div className="project card" key={project.title}>
-            <div className="project-content">
+        <div 
+            className="project card" 
+            style={{
+                backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${images[0]})`,
+                backgroundPosition: 'center',
+                backgroundSize: 'cover',
+                backgroundRepeat: 'no-repeat'
+            }}
+            key={project.title}
+        >
+            <div 
+                className="project-content"
+            >
                 <h2>{project.title}</h2>
-                <p>{project.description}</p>
-                <ul className="techStack">
+                <h4>{project.description}</h4>
+                {/* <ul className="techStack">
                     {techStack.map(item => <li key={item}>{item}</li>)}
-                </ul>
+                </ul> */}
             </div>
-            <div className='images-container'>
-                {images.map(image => <img key={image} src={image} alt='project-image' className='image'/>)}
-            </div>
+            {/* <div className='images-container'>
+                <img src={images[0]} alt='project-image' className='image'/>
+                {/* {images.map(image => <img key={image} src={image} alt='project-image' className='image'/>)} 
+            </div> */}
         </div>
     )
 }
