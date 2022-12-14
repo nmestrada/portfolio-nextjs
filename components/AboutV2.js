4a249165553a2c456cb3efbb286afdb41c23ea17
Natalie Estrada <nmestrad@gmail.com>
2022-12-13 21:36:26 -0600

    Hecking lot of changes commented out old styles

---
+++ components/AboutV2.js
@@ -0,0 +1,14 @@
+export default function AboutMe() {
+  return (
+    <div className="about">
+      <h1>Hello!</h1>
+      <img src="/images/headshoticon1.jpg" alt="NatalieEstradaImage" />
+      <h2>I'm Natalie a Software Engineer</h2>
+      <p>
+        I code in JavaScript/TypeScript and specialize in React and React Native
+        techonologies
+      </p>
+      <p>I enjoy learning new things and sharing knowledge!</p>
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,36 @@
-import Head from 'next/head'
-import axios from 'axios'
-import {useState, useEffect} from 'react'
-import SingleProject from '../components/SingleProject'
-import About from '../components/About'
-import Scrollspy from 'react-scrollspy'
+//import Head from 'next/head';
+//import axios from 'axios';
+//import { useState, useEffect } from 'react';
+//import SingleProject from '../components/SingleProject';
+import AboutV2 from '../components/AboutV2';
+//import Scrollspy from 'react-scrollspy';
 
 export default function Home() {
-  const [projects, setProjects] = useState([])
-  const [navClass, setNavClass] = useState('nav')
-  useEffect(()=>{
-    axios.get('/api/projects')
-    .then( res => {
-        setProjects(res.data)
-    })
-    .catch(err => console.log(err.message))
-    window.addEventListener('scroll', function() {
-        // add throttle instead this calllback
-        let heroHeight = document.querySelector('.hero').clientHeight;
-        console.log('hero height',heroHeight)
-        if(window.scrollY > heroHeight){
-            setNavClass('nav solid')
-        }else {
-            setNavClass('nav')
-        }
-    })
-  },[])
+  //const [projects, setProjects] = useState([]);
+  //const [navClass, setNavClass] = useState('nav');
+  //   useEffect(() => {
+  //     axios
+  //       .get('/api/projects')
+  //       .then((res) => {
+  //         setProjects(res.data);
+  //       })
+  //       .catch((err) => console.log(err.message));
+  //     window.addEventListener('scroll', function () {
+  //       // add throttle instead this calllback
+  //       let heroHeight = document.querySelector('.hero').clientHeight;
+  //       console.log('hero height', heroHeight);
+  //       if (window.scrollY > heroHeight) {
+  //         setNavClass('nav solid');
+  //       } else {
+  //         setNavClass('nav');
+  //       }
+  //     });
+  //   }, []);
   //scrolling navbar control
-  
-  
+
   return (
     <div className="container">
-      <Head>
+      {/* <Head>
         <title>Natalie Estrada Full Stack Developer</title>
         <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap" rel="stylesheet"></link>
       </Head>
@@ -40,28 +40,28 @@ export default function Home() {
         <a href="#about">About</a>
       </Scrollspy>
       
-    </nav>
+    </nav> */}
       <main>
-      <div className="hero">
-            <h1>Natalie Estrada</h1>
-            <h2>Software Developer</h2>
+        {/* <div className="hero">
+          <h1>Natalie Estrada</h1>
+          <h2>Software Developer</h2>
         </div>
         <h1>Projects</h1>
         <section className="projects" id="projects">
-            {
-                projects.map(project => <SingleProject project={project}/>)
-            }
-        </section>
-        <About/>
+          {projects.map((project) => (
+            <SingleProject project={project} />
+          ))}
+        </section> */}
+        <AboutV2 />
         <section className="blog">
-            <h2>Blog</h2>
-            <p>Currently in Progress!</p>
+          <h2>Blog</h2>
+          <p>Currently in Progress!</p>
         </section>
       </main>
 
       <footer>
-        <p>	&copy; {new Date().getFullYear()} Natalie Estrada</p>
+        <p> &copy; {new Date().getFullYear()} Natalie Estrada</p>
       </footer>
     </div>
-  )
+  );
 }
