3d55b47966ef7dc79eb892f05b2b94fde548e9a3
Natalie Estrada <nmestrad@gmail.com>
2023-09-21 20:21:45 -0500

    Comitting stuff

--- components/Blog.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-export const Blog = ({content,title}) => (
-    <article>
-        <h3>{title}</h3>
-        <p>{content}</p>
-    </article>
-)
--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,30 @@
 //import Head from 'next/head';
-//import axios from 'axios';
-//import { useState, useEffect } from 'react';
-//import SingleProject from '../components/SingleProject';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 import AboutV2 from '../components/AboutV2';
-import { Blog } from '../components/Blog';
-//import Scrollspy from 'react-scrollspy';
+import Post from '../components/Post/Post';
 
 export default function Home() {
-  //const [projects, setProjects] = useState([]);
+  const [posts, setPosts] = useState([]);
   //const [navClass, setNavClass] = useState('nav');
-  //   useEffect(() => {
-  //     axios
-  //       .get('/api/projects')
-  //       .then((res) => {
-  //         setProjects(res.data);
-  //       })
-  //       .catch((err) => console.log(err.message));
-  //     window.addEventListener('scroll', function () {
-  //       // add throttle instead this calllback
-  //       let heroHeight = document.querySelector('.hero').clientHeight;
-  //       console.log('hero height', heroHeight);
-  //       if (window.scrollY > heroHeight) {
-  //         setNavClass('nav solid');
-  //       } else {
-  //         setNavClass('nav');
-  //       }
-  //     });
-  //   }, []);
+  useEffect(() => {
+    axios
+      .get('/api/blogPosts')
+      .then((res) => {
+        setPosts(res.data);
+      })
+      .catch((err) => console.log(err.message));
+    //   window.addEventListener('scroll', function () {
+    //     // add throttle instead this calllback
+    //     let heroHeight = document.querySelector('.hero').clientHeight;
+    //     console.log('hero height', heroHeight);
+    //     if (window.scrollY > heroHeight) {
+    //       setNavClass('nav solid');
+    //     } else {
+    //       setNavClass('nav');
+    //     }
+    //   });
+  }, []);
   //scrolling navbar control
 
   return (
@@ -34,7 +32,10 @@ export default function Home() {
       <main>
         <AboutV2 />
         <section className="blog">
-          <Blog />
+          <h2>Blog Posts</h2>
+          {posts.map((post) => (
+            <Post {...post} />
+          ))}
         </section>
       </main>
 
