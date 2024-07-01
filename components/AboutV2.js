20d698efd951cdb928b99e7c2f3340a48e76ea81
Natalie Estrada <nmestrad@gmail.com>
2024-06-30 21:37:19 -0500

    adding tailwind styles fixing the styles that were not applying

--- components/AboutV2.js
+++ components/AboutV2.js
@@ -2,29 +2,29 @@ export default function AboutMe() {
   return (
     <div className="">
       <nav>
-        <div class="border-b border-gray-200">
+        <div className="border-b border-gray-200">
           <div
-            class="-mb-px flex space-x-8 px-4"
+            className="-mb-px flex space-x-8 px-4"
             aria-orientation="horizontal"
             role="tablist"
           >
             <button
               id="tabs-1-tab-1"
-              class="flex-1 whitespace-nowrap border-b-2 border-transparent px-1 py-4 text-base font-medium text-gray-900"
+              className="flex-1 whitespace-nowrap border-b-2 border-transparent px-1 py-4 text-base font-medium text-gray-900"
               aria-controls="tabs-1-panel-1"
               role="tab"
               type="button"
             >
-              Women
+              Blog
             </button>
             <button
               id="tabs-1-tab-2"
-              class="flex-1 whitespace-nowrap border-b-2 border-transparent px-1 py-4 text-base font-medium text-gray-900"
+              className="flex-1 whitespace-nowrap border-b-2 border-transparent px-1 py-4 text-base font-medium text-gray-900"
               aria-controls="tabs-1-panel-2"
               role="tab"
               type="button"
             >
-              Men
+              My Projects
             </button>
           </div>
         </div>
